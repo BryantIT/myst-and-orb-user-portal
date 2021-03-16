34be045dfa5e9d4b3d8e9ae4c5c2071894ad178a
Bryant Richards <dragonseyeimages@gmail.com>
2021-03-16 03:54:36 -0500

    removed orbs because of memory usage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import React , { Fragment } from 'react'
+import React , { Fragment, useState, useEffect } from 'react'
 // Components
-import Orbs from './components/orbs/Orbs'
 import Guests from './components/guests/Guests'
 import Splash from './components/splash/Splash'
 import logo from './logo.svg'
 import './App.css'
 import { Route, Switch, withRouter  } from 'react-router-dom'
+// Database
+import db from './firebase'
 
 function App() {
+  const [info, setInfo] = useState('')
+
+  const getInfo = () => {
+    db.collection('information').onSnapshot((snapshot) => {
+      setInfo(snapshot.docs.map((doc) => {
+        return {
+          id: doc.id,
+          about: doc.data().about
+        }
+      }))
+    })
+  }
+
+  useEffect(() => {
+    getInfo()
+  }, [])
+
   return (
     <Fragment>
-      <Orbs />
       <Switch>
         <Route exact path='/' component={Splash} />
         <Route exact path='/guests' component={Guests} />
