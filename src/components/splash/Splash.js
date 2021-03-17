0582e20e5adc5c7ff661e157eb98ac9ce33f32ef
Bryant Richards <dragonseyeimages@gmail.com>
2021-03-17 14:47:42 -0500

    removed unused JS

--- src/components/splash/Splash.js
+++ src/components/splash/Splash.js
@@ -1,35 +1,32 @@
 import React, { Fragment } from 'react'
 // Styles
-import { BottomDivider, TopDivider } from './Styles'
+import {
+  BottomDivider,
+  TopDivider,
+  Main,
+  Vignette,
+  TitleContent,
+  MainTitle,
+  StyledLink } from './Styles'
 import './style.css'
-import { NavLink } from 'react-router-dom'
-import { Helmet } from 'react-helmet'
 
 const Splash = () => {
   return(
     <Fragment>
-      <Helmet>
-        <script src="https://cdnjs.﻿cloudflare.com/ajax/libs/gsap/2.0.1/TweenMax.min.js"></script>﻿
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.0.1/TimelineMax.min.js"></script>
-        <script src="js/background-noise.js"></script>
-        <script src="js/old-movie-style.js"></script>
-      </Helmet>
-
-      <main role="main" className="main-content" id="main-content">
+      <Main role='main' id='main-content'>
         <TopDivider src='images/divider.svg' alt='Home' />
-        <h1 className="main-title">Manes daemones spirituum, O mi.</h1>
-      <div className="titleCont">
-          <h1 className="main-title" id="main-title">
-              "Through the dark,<br /><span style={{ paddingLeft: 100 }}>the most haunted,</span><br /><span style={{ paddingRight:110 }}>where all fear to tread,</span><br /><span style={{ paddingLeft: -20 }}>the Myst & Orb Society."</span>
-          </h1>
-      </div>
-      <canvas id="noise" className="noise"></canvas>
-    <div className="vignette"></div>
+        <MainTitle>Manes daemones spirituum, O mi.</MainTitle>
+      <TitleContent>
+          <MainTitle id="main-title">
+              "Through the dark,<br /><span style={{ paddingLeft: 100 }}>the most haunted,</span><br /><span style={{ paddingRight:110 }}>where all fear to tread,</span><br /><span style={{ paddingLeft: 120 }}>the Myst & Orb Society."</span>
+          </MainTitle>
+      </TitleContent>
+    <Vignette />
     <BottomDivider src='images/divider.svg' alt='Home' />
-    <div>
-      <NavLink to='/home' className="main-title" id="main-title">Enter</NavLink>
-    </div>
-  </main>
+    <MainTitle>
+      <StyledLink to='/home' id="main-title">Enter</StyledLink>
+    </MainTitle>
+      </Main>
     </Fragment>
   )
 }
--- src/components/splash/Styles.js
+++ src/components/splash/Styles.js
@@ -1,4 +1,5 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
+import { NavLink } from 'react-router-dom'
 
 export const BottomDivider = styled.img`
   width: 50%;
@@ -7,4 +8,87 @@ export const BottomDivider = styled.img`
 export const TopDivider = styled.img`
   width: 50%;
   transform: scaleY(-1)
+`
+const grainAnimation = keyframes`
+0%, 100% { transform:translate(0, 0) }
+10% { transform:translate(-5%, -10%) }
+20% { transform:translate(-15%, 5%) }
+30% { transform:translate(7%, -25%) }
+40% { transform:translate(-5%, 25%) }
+50% { transform:translate(-15%, 10%) }
+60% { transform:translate(15%, 0%) }
+70% { transform:translate(0%, 15%) }
+80% { transform:translate(3%, 35%) }
+90% { transform:translate(-10%, 10%) }
+`
+
+export const Main = styled.main`
+  overflow:hidden;
+  position: relative;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-flow: column;
+
+  height: 100vh;
+
+  background: linear-gradient(to right, rgba(36,31,31,1) 0%, rgba(36,31,31,1) 32%, rgba(74,71,70,1) 100%);
+  color: #fff;
+  text-align: center;
+  &:after {
+  animation: ${grainAnimation} 20s steps(10) infinite;
+  background-image: url('images/vintage_speckles.png');
+  content: "";
+  height: 300%;
+  left: -50%;
+  opacity: 0.5;
+  position: fixed;
+  top: -100%;
+  width: 300%;
+  }
+`
+
+const vignetteAnimation = keyframes`
+  0%,  100%{ opacity: 1; }
+  50% { opacity: 0.7; }
+`
+
+export const Vignette = styled.div`
+  position:absolute;
+  width:100%; height:100%;
+  box-shadow:inset 0px 0px 150px 20px black;
+  mix-blend-mode: multiply;
+  -webkit-animation: ${vignetteAnimation} 5s infinite; /* Safari 4+ */
+  -moz-animation:    ${vignetteAnimation} 5s infinite; /* Fx 5+ */
+  -o-animation:      ${vignetteAnimation} 5s infinite; /* Opera 12+ */
+  animation:         ${vignetteAnimation} 5s infinite; /* IE 10+, Fx 29+ */
+`
+
+export const Grain = styled.div`
+  position:relative;
+`
+
+export const TitleContent = styled.div`
+  position:relative;
+`
+
+export const MainTitle = styled.h1`
+  padding: .3em 1em .25em;
+  font-weight: 400;
+  font-size: 40px;
+  color: white;
+  font-family: 'Times New Roman', serif;
+  position:relative;
+  line-height:1.3;
+`
+
+export const StyledLink = styled(NavLink)`
+  padding: .3em 1em .25em;
+  font-weight: 400;
+  font-size: 40px;
+  color: white;
+  font-family: 'Times New Roman', serif;
+  position:relative;
+  line-height:1.3;
 `
