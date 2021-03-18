350b66253d8c80666454d5460fea508d83bfef9d
Bryant Richards <dragonseyeimages@gmail.com>
2021-03-18 06:37:00 -0500

    added a font

--- src/components/general/Styles.js
+++ src/components/general/Styles.js
@@ -1,11 +1,13 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-
+  margin: auto;
+  width: 80%;
 `
 
 export const WelcomeContainer = styled.div`
-
+  text-align: center;
+  padding-bottom: 10px;
 `
 
 export const AboutContainer = styled.div`
