9870290a398ac94a8b198344c5270738f666bcf8
Bryant Richards <dragonseyeimages@gmail.com>
2021-03-24 11:26:21 -0500

    user logic for dashboard

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import { useAuth } from '../../auth/UserAuth'
 
 const Dashboard = () => {
+  const { currentUser } = useAuth()
   return (
+    !currentUser ? (
+      <div>Not a Current User, please sign up</div>
+    ) :
     <div>Hello Dashboard</div>
   )
 }
